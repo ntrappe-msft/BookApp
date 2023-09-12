31b8fff961786c2829b2eac8e94f4243d6823a0e
Nicole Trappe <124631722+ntrappe-msft@users.noreply.github.com>
2023-09-11 22:21:58 -0700

    Dependency refactor (#8)
    * testing works
    
    * cypress tests auto
    
    * book helper fxn tests working
    
    * tested defaults
    
    * Book has defaults and can set info
    
    * remove extra file
    
    * adding back new dependencies and cyprus first
    
    * remove screenshots
    
    ---------
    
    Co-authored-by: Microsoft <MSFT@Nicoles-Chonk.local>

---
+++ cypress.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+    component: {
+        // specPattern: 'cypress/component/**/*.cy.{ts,tsx}',
+        devServer: {
+            framework: "react",
+            bundler: "vite",
+        },
+    },
+});
