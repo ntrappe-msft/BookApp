692c4910761e8aa123025ce4fa11b7d82ed73da7
Microsoft <MSFT@Nicoles-Chonk.local>
2023-09-11 22:07:37 -0700

    adding back new dependencies and cyprus first

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
     component: {
-        specPattern: 'cypress/component/**/*.cy.{ts,tsx}',
+        // specPattern: 'cypress/component/**/*.cy.{ts,tsx}',
         devServer: {
             framework: "react",
             bundler: "vite",
